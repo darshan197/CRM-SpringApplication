6820d544d3d59e18738f1fd4e485983d95ead13d
Darshan Hosakote <darshanhosakote@Darshans-MBP.fios-router.home>
2017-06-13 17:38:55 -0400

    Initial Commit

---
+++ src/com/darshan/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.darshan.springdemo.dao;
+
+import java.util.List;
+
+import com.darshan.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
---
+++ src/com/darshan/test/db/TestDBServelet.java
@@ -0,0 +1,52 @@
+package com.darshan.test.db;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ * Servlet implementation class TestDBServelet
+ */
+@WebServlet("/TestDBServelet")
+public class TestDBServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//Setup connection vars
+		String user = "springstudent";
+		String pwd = "springstudent";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		
+		String driver = "com.mysql.jdbc.Driver";
+		
+		//Get connection to DB
+		try {
+			
+			PrintWriter out = response.getWriter();
+			out.println("Connecting to DB:"+jdbcUrl);
+			Class.forName(driver);
+			
+			Connection myCon = DriverManager.getConnection(jdbcUrl,user,pwd);
+			
+			out.println("Done!");
+			myCon.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServletException();
+		}
+		
+		
+		
+	}
+
+}
